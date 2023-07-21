5d3f873ffbee9e5718879682e93f341e3e6c5663
DrEden33773 <edwardwang33773@gmail.com>
2023-07-22 00:28:38 +0800

    update

--- src/bit_ops/mod.rs
+++ src/bit_ops/mod.rs
@@ -12,3 +12,36 @@ pub trait BitwiseOperative: Sized {
     fn count_one(&self) -> usize;
     fn count_zero(&self) -> usize;
 }
+
+impl BitwiseOperative for usize {
+    fn set_zero_on(&mut self, positions: &[usize]) {
+        for position in positions {
+            *self &= !(1 << position)
+        }
+    }
+    fn set_one_on(&mut self, positions: &[usize]) {
+        for position in positions {
+            *self |= 1 << position
+        }
+    }
+    fn count_one(&self) -> usize {
+        let how_many_bits = std::mem::size_of::<usize>() * 8;
+        let mut count = 0;
+        for i in 0..how_many_bits {
+            if self & (1 << i) != 0 {
+                count += 1;
+            }
+        }
+        count
+    }
+    fn count_zero(&self) -> usize {
+        let how_many_bits = std::mem::size_of::<usize>() * 8;
+        let mut count = 0;
+        for i in 0..how_many_bits {
+            if self & (1 << i) == 0 {
+                count += 1;
+            }
+        }
+        count
+    }
+}
